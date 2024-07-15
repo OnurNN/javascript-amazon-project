66b99a579e8ecb3447e65501eb4bec5e0c12f51c
OnurNN <uchihamdrase@gmail.com>
2024-07-15 13:47:13 +0300

    use product js...2

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,32 +1,28 @@
-  let productsHTML = '';
-
-    products.forEach((product) => {
-      productsHTML += `
-        <div class="product-container">
+    let productHTML=''
+  products.forEach((product) =>{
+    productHTML +=`<div class="product-container">
           <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
+            <img class="product-image" src="${product.image}">
           </div>
-    
+
           <div class="product-name limit-text-to-2-lines">
             ${product.name}
           </div>
-    
+
           <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
-    
+
           <div class="product-price">
             $${(product.priceCents / 100).toFixed(2)}
           </div>
-    
+
           <div class="product-quantity-container">
             <select>
-              <option selected value="1">1</option>
+              <option selected="" value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
@@ -38,20 +34,20 @@
               <option value="10">10</option>
             </select>
           </div>
-    
+
           <div class="product-spacer"></div>
-    
+
           <div class="added-to-cart">
             <img src="images/icons/checkmark.png">
             Added
           </div>
-    
-          <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-id="${product.id}">
+
+          <button class="add-to-cart-button button-primary">
             Add to Cart
           </button>
-        </div>
-      `;
-      
-    });
-   
+        </div>`
+        
+  });
+
+  
+  document.querySelector('.js-products-grid').innerHTML =productHTML;
