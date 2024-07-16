49d5595851d2673e7142727c33d7d946c7587d00
OnurNN <uchihamdrase@gmail.com>
2024-07-17 00:53:53 +0300

    save data and generate html for payment

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,2 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
-renderOrderSummary;
+renderOrderSummary();
+
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+renderPaymentSummary();
