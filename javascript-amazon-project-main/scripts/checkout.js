97a409593dd61a800525b0a02e86b31b87e52171
OnurNN <uchihamdrase@gmail.com>
2024-07-18 20:59:28 +0300

    conver from promises to async await

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,6 +6,28 @@ import { loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 
+async function loadPage(){
+   
+
+    await loadProductsFetch();
+
+    const value =await new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve('value3');
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+    
+}
+loadPage();
+
+
+
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve)=>{
@@ -20,7 +42,7 @@ Promise.all([
     renderPaymentSummary();
 
 });
-
+*/
 
 /*
 new Promise((resolve)=>{
